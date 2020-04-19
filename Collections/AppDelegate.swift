231cc08d517661a957749629a56db6c0f6bc512e
Dan_Alboteanu <alboteanud@gmail.com>
2020-04-19 14:03:57 +0300

    first commit

--- Collections/AppDelegate.swift
+++ Collections/AppDelegate.swift
@@ -7,31 +7,23 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+  
+        FirebaseApp.configure()
+
+        // Globally set navigation bar style
+        let navigationStyles = UINavigationBar.appearance()
+        navigationStyles.barTintColor =
+          UIColor(red: 0x3d/0xff, green: 0x5a/0xff, blue: 0xfe/0xff, alpha: 1.0)
+        navigationStyles.tintColor = UIColor(white: 0.8, alpha: 1.0)
+            navigationStyles.titleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor.white]
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
