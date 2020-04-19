947fe081b6d50b650d08ea132460c8a56701cb33
Dan_Alboteanu <alboteanud@gmail.com>
2020-04-19 12:39:27 +0300

    Initial Commit

---
+++ Collections/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collections
+//
+//  Created by Dan Alboteanu on 19/04/2020.
+//  Copyright © 2020 Dan Alboteanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
